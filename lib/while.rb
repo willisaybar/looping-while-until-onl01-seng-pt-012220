53128407743044627b45a2c275dc7c6db289fad5
Willis Aybar <willisaybar@gmail.com>
2020-01-30 01:20:37 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
-def using_while("Wingardium Leviosa")
+def using_while(levitation_force)
   levitation_force = 6
-	while using_while < 10 
+	while levitation_force < 10 
 	puts "Wingardium Leviosa"
   using_while += 1 
 end

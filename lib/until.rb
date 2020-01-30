d978f0a22998faaf6e44e310ed3f223475502def
Willis Aybar <willisaybar@gmail.com>
2020-01-30 01:25:50 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
   puts "Wingardium Leviosa"
-  while levitation_force <10 
+  while levitation_force < 10 
   until levitation_force == 10
   levitation_force += 1
 end
